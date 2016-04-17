bd34e2b78af280008f4111e2402b4238968861ad
drrick42 <drrick42@gmail.com>
2016-04-17 11:13:27 -0400

    Added background and planet alignment
    set the planets and sun to stay equal distance from the player, added
    distant stars, and set the planets and star to slowly rotate

---
+++ Assets/Scripts/solar_controller.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class solar_controller : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 init_pos;
+    private float x_change = 0;
+    private float y_change = 0;
+    private float z_change = 0;
+
+    // Use this for initialization
+    void Start () {
+        init_pos = transform.position;
+        player = GameObject.Find("drone");
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = init_pos + player.transform.position;
+        transform.position = pos;
+        x_change = init_pos.x - pos.x;
+        y_change = init_pos.y - pos.y;
+        z_change = init_pos.z - pos.z;
+    }
+}
