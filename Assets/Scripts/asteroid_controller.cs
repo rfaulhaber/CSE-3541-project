d842bd7e3526ee03d7131f6159eb69b0fece0f02
drrick42 <drrick42@gmail.com>
2016-04-19 14:43:32 -0400

    Implement Partial Asteroid AI
    Need to make it so asteroids generate randomly, and so that they drift
    around, but they are collidable, and they break into smaller asteroids
    when shot.

---
+++ Assets/Scripts/asteroid_controller.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class asteroid_controller : MonoBehaviour {
+
+    public Vector3 rotation;
+    public GameObject small_asteroid_one, small_asteroid_two, explosion;
+
+    // Use this for initialization
+    void Start () {
+        rotation = new Vector3(Random.Range(-.3F, .3F), Random.Range(-.3F, .3F), Random.Range(-.3F, .3F));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(rotation);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Projectile(Clone)")
+        {
+            Instantiate(explosion, gameObject.transform.position, Quaternion.identity);
+            MakeSmallAsteroid();
+            MakeSmallAsteroid();
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    void MakeSmallAsteroid()
+    {
+        if (Random.Range(1, 3) == 1)
+        {
+            Instantiate(small_asteroid_one, gameObject.transform.position, gameObject.transform.rotation);
+        }
+        else
+        {
+            Instantiate(small_asteroid_two, gameObject.transform.position, gameObject.transform.rotation);
+        }
+    }
+
+}
---
+++ Assets/Scripts/small_asteroid_controller.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class small_asteroid_controller : MonoBehaviour {
+
+    public Vector3 rotation;
+    public GameObject explosion;
+
+    // Use this for initialization
+    void Start () {
+        rotation = new Vector3(Random.Range(-.7F, .7F), Random.Range(-.7F, .7F), Random.Range(-.7F, .7F));
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(rotation);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Projectile(Clone)")
+        {
+            Instantiate(explosion, gameObject.transform.position, Quaternion.identity);
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
